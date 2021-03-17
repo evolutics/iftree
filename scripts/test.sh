f2fbc0d80314904220273604f0f43466274cd104
Benjamin Fischer <benjamin.fischer@evolutics.info>
2021-03-17 18:59:45 +0100

    Test Rust code

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,6 +4,17 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+test_rust() {
+  rustup component add rustfmt
+  cargo fmt --all -- --check
+
+  rustup component add clippy
+  cargo clippy --all-features --all-targets -- --deny warnings
+
+  cargo check
+  cargo test
+}
+
 main() {
   local -r script_folder="$(dirname "$(readlink --canonicalize "$0")")"
   local -r project_folder="$(dirname "${script_folder}")"
@@ -12,6 +23,8 @@ main() {
   docker run --entrypoint sh --rm --volume "$(pwd)":/workdir \
     evolutics/travel-kit:0.6.0 -c \
     'git ls-files -z | xargs -0 travel-kit check --'
+
+  test_rust
 }
 
 main "$@"
