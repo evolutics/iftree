b41b73ce5b322291b6ef128f471427b268010c7a
Benjamin Fischer <benjamin.fischer@evolutics.info>
2021-03-17 19:05:09 +0100

    Change generated example code for Clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
+pub fn magic() -> i32 {
+    4
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        assert_eq!(magic(), 4);
     }
 }
