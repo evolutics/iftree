0708b66368c368fb964c68b4f46a6cbc69f7e7fd
Benjamin Fischer <benjamin.fischer@evolutics.info>
2021-03-21 18:57:59 +0100

    Generate suggestions for other identifiers in case of collisions

--- src/index_files.rs
+++ src/index_files.rs
@@ -1,3 +1,4 @@
+mod generate_identifier;
 mod main;
 mod sanitize_to_identifier;
 
---
+++ src/index_files/generate_identifier.rs
@@ -0,0 +1,23 @@
+#[allow(dead_code)]
+pub fn main(base: &str, is_valid: &dyn Fn(&str) -> bool) -> String {
+    let mut index = 0;
+    loop {
+        let identifier = format!("{}{}", base, index);
+        if is_valid(&identifier) {
+            break identifier;
+        }
+        index += 1;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn generates() {
+        let actual = main("zeta", &|name| name.len() > 5);
+
+        assert_eq!(actual, "zeta10");
+    }
+}
