6f348693972f1bb01fd6c1ab5930a8ced55fea61
Benjamin Fischer <benjamin.fischer@evolutics.info>
2021-03-22 07:54:27 +0100

    Rename to clarify what kind of path

--- src/index_files.rs
+++ src/index_files.rs
@@ -1,7 +1,7 @@
 mod generate_identifier;
+mod get_canonical_paths;
 mod get_files;
 mod get_forest;
-mod get_paths;
 mod main;
 mod sanitize_to_identifier;
 
--- src/index_files/main.rs
+++ src/index_files/main.rs
@@ -1,10 +1,10 @@
+use super::get_canonical_paths;
 use super::get_files;
 use super::get_forest;
-use super::get_paths;
 use crate::model;
 
 pub fn main(resource_type: model::TypeAlias) -> model::FileIndex {
-    let canonical_paths = get_paths::main();
+    let canonical_paths = get_canonical_paths::main();
     let files = get_files::main(canonical_paths);
     let forest = get_forest::main(files);
     model::FileIndex {
