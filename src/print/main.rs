1187ed29d92f8668cd39011fa6a922a31c37783e
Benjamin Fischer <benjamin.fischer@evolutics.info>
2021-03-28 14:36:04 +0200

    Test printing as whole

--- src/print/main.rs
+++ src/print/main.rs
@@ -15,3 +15,45 @@ pub fn main(
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::path;
+
+    #[test]
+    fn prints() {
+        let item = quote::quote! {
+            pub type Resource = &'static str;
+        };
+        let mut forest = model::FileForest::new();
+        forest.insert(
+            String::from("CREDITS_MD"),
+            model::FileTree::File(model::File {
+                absolute_path: path::PathBuf::from("/credits.md"),
+                ..model::stubs::file()
+            }),
+        );
+
+        let actual = main(
+            item,
+            model::FileIndex {
+                resource_type: String::from("Resource"),
+                forest,
+            },
+        );
+
+        let actual = actual.to_string();
+        let expected = quote::quote! {
+            pub type Resource = &'static str;
+
+            pub mod root {
+                use super::Resource;
+
+                pub const CREDITS_MD: Resource = include_str!("/credits.md");
+            }
+        }
+        .to_string();
+        assert_eq!(actual, expected);
+    }
+}
