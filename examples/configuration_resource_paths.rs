5737d2ca818c5c60d454395c07dcf0a0fd92b7bd
Benjamin Fischer <benjamin.fischer@evolutics.info>
2021-04-04 19:23:46 +0200

    Add example for resource path patterns

---
+++ examples/configuration_resource_paths.rs
@@ -0,0 +1,26 @@
+#[files_embedded_as_modules::embed_files_as_modules(
+    "
+resource_paths = '''
+/examples/resources/**
+!/examples/resources/world/
+/README.md
+!.*
+'''
+
+[field_templates]
+_ = '{{relative_path}}'
+"
+)]
+pub type Resource = &'static str;
+
+pub fn main() {
+    assert_eq!(
+        ARRAY,
+        [
+            &"README.md",
+            &"examples/resources/configuration/menu.json",
+            &"examples/resources/configuration/translations.csv",
+            &"examples/resources/credits.md",
+        ],
+    );
+}
