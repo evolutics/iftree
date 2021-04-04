f1b0edec73654904fb95a6b96c034d415abdbe94
Benjamin Fischer <benjamin.fischer@evolutics.info>
2021-04-04 10:51:31 +0200

    Rewrite in better suited scripting language

---
+++ scripts/test.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import os
+import pathlib
+import subprocess
+
+
+def main():
+    os.chdir(pathlib.Path(os.path.realpath(__file__)).parent.parent)
+
+    working_folder = pathlib.Path.cwd()
+    subprocess.run(
+        [
+            "docker",
+            "run",
+            "--entrypoint",
+            "sh",
+            "--rm",
+            "--volume",
+            f"{working_folder}:/workdir",
+            "evolutics/travel-kit:0.6.0",
+            "-c",
+            "git ls-files -z | xargs -0 travel-kit check --",
+        ],
+        check=True,
+    )
+
+    _test_rust()
+
+
+def _test_rust():
+    subprocess.run(["rustup", "component", "add", "rustfmt"], check=True)
+    subprocess.run(["cargo", "fmt", "--all", "--", "--check"], check=True)
+
+    subprocess.run(["rustup", "component", "add", "clippy"], check=True)
+    subprocess.run(
+        [
+            "cargo",
+            "clippy",
+            "--all-features",
+            "--all-targets",
+            "--",
+            "--deny",
+            "warnings",
+        ],
+        check=True,
+    )
+
+    subprocess.run(["cargo", "check"], check=True)
+    subprocess.run(["cargo", "test"], check=True)
+
+    subprocess.run(["cargo", "run", "--example", "main"], check=True)
+
+
+if __name__ == "__main__":
+    main()
