add0f57dc696c7e42b15072108a8a4d2e48adefe
Benjamin Fischer <benjamin.fischer@evolutics.info>
2021-04-09 00:01:30 +0200

    Demonstrate field template that depends on build profile

---
+++ examples/scenario_debug_versus_release_builds.rs
@@ -0,0 +1,34 @@
+#[cfg(debug_assertions)]
+macro_rules! load {
+    ($relative_path:literal, $absolute_path:literal) => {
+        concat!("Debug: ", include_str!($absolute_path))
+    };
+}
+
+#[cfg(not(debug_assertions))]
+macro_rules! load {
+    ($relative_path:literal, $absolute_path:literal) => {
+        concat!("Release: ", include_str!($absolute_path))
+    };
+}
+
+#[files_embedded_as_modules::embed_files_as_modules(
+    "
+resource_paths = 'examples/resources/credits.md'
+
+[field_templates]
+data = 'load!'
+"
+)]
+pub struct Resource {
+    data: &'static str,
+}
+
+pub fn main() {
+    use base::examples::resources;
+
+    assert!(
+        (resources::CREDITS_MD.data == "Debug: Foo Bar\n")
+            || (resources::CREDITS_MD.data == "Release: Foo Bar\n")
+    );
+}
