da95ae098e386ce744c47ed5a0b53590aae48492
Benjamin Fischer <benjamin.fischer@evolutics.info>
2021-04-10 19:28:55 +0200

    Document feature overview

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,9 @@
 //! assert_eq!(base::my_resources::subfolder::FILE_C.content, "… contents of `file_c`\n");
 //! ```
 //!
+//! As you can see, folders are mapped to (nested) modules, which are rooted at a
+//! top-level module `base`.
+//!
 //! For this to work, you can call the library as in
 //!
 //! ```ignore
@@ -35,10 +38,37 @@
 //!
 //! This calls the macro `iftree::include_file_tree` on a custom type `Resource`.
 //! The argument defines a path pattern that configures which files to include, in
-//! this case the files in the folder `my_resources`, including its subfolders. For
-//! each such file, an instance of `Resource` is initialized with the fields given
-//! by `Resource`. The well-known field `content` is initialized with a call to
-//! `include_str!`, but you can provide your own macros to initialize a field.
+//! this case the files in the folder `my_resources` and its subfolders. For each
+//! such file, an instance of `Resource` is initialized with the fields given by
+//! `Resource`. The well-known field `content` is initialized with a call to
+//! `include_str!`, but you can provide your own templates to initialize a field.
+//!
+//! # Feature overview
+//!
+//! There is an
+//! [**`examples` folder**](https://github.com/evolutics/iftree/tree/main/examples)
+//! with full code examples to demonstrate the following main aspects.
+//!
+//! The annotated **resource type** (`Resource` above) can be a `struct` with any
+//! number of fields. Alternatively, it can be a type alias – especially convenient
+//! if there is only one field.
+//!
+//! To **filter files,** path patterns in a `.gitignore`-like format are supported.
+//! This is useful to skip hidden files, filter by filename extension, add multiple
+//! folders, etc. See the `resource_paths` configuration for more.
+//!
+//! **Field templates** are applied to initialize fields. The standard case is to
+//! include the file contents as code. Among other predefined templates there is one
+//! that includes the file contents only in release builds, while in debug builds it
+//! reads a file afresh on each access. See the `field_templates` configuration for
+//! more.
+//!
+//! **Custom field templates** enable plugging in your own macros to initialize
+//! fields. With this, you could add file metadata like media types, compress a file
+//! when including it, etc.
+//!
+//! To **iterate** over the generated resources, see the `generate_array`
+//! configuration.
 
 mod data;
 mod index_files;
