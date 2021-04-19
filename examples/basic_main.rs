f3f306e13beb52ce12d616d63f30d3777abbc785
Benjamin Fischer <benjamin.fischer@evolutics.info>
2021-04-19 16:37:47 +0200

    Reduce to single first example for simplicity

--- examples/basic_main.rs
+++ examples/basic_main.rs
@@ -1,18 +1,17 @@
-#[iftree::include_file_tree("paths = '/examples/assets/**'")]
+// Introductory example of `README.md`.
+
+#[iftree::include_file_tree("paths = '/my_assets/**'")]
 pub struct Asset {
     content: &'static str,
 }
 
 pub fn main() {
-    use base::examples::assets;
-
-    assert_eq!(assets::_ENV.content, "BASE=https://example.com\n");
-    assert_eq!(assets::configuration::MENU_JSON.content, "\"Start\"\n");
-    assert_eq!(assets::configuration::TRANSLATIONS_CSV.content, "Back\n");
-    assert_eq!(assets::CREDITS_MD.content, "Boo Far\n");
-    assert_eq!(assets::world::levels::TUTORIAL_JSON.content, "\"Hi\"\n");
-    assert_eq!(assets::world::PHYSICAL_CONSTANTS_JSON.content, "7e-3\n");
-
-    assert_eq!(ASSETS.len(), 6);
-    assert_eq!(ASSETS[3].content, "Boo Far\n");
+    assert_eq!(base::my_assets::FILE_A.content, "… contents of `file_a`\n");
+    assert_eq!(base::my_assets::FILE_B.content, "… contents of `file_b`\n");
+    assert_eq!(
+        base::my_assets::subfolder::FILE_C.content,
+        "… and of `file_c`\n",
+    );
+    assert_eq!(ASSETS.len(), 3);
+    assert_eq!(ASSETS[0].content, "… contents of `file_a`\n");
 }
