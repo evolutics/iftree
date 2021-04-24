f381a7350e866bcdce8e3a384ea31c17b7db9f50
Benjamin Fischer <benjamin.fischer@evolutics.info>
2021-04-24 22:20:18 +0200

    Reduce example to relevant library only by using local function

--- examples/library_mime_guess.rs
+++ examples/library_mime_guess.rs
@@ -1,9 +1,12 @@
-use once_cell::sync;
-
 macro_rules! initialize {
     ($relative_path:literal, $absolute_path:literal) => {
         Asset {
-            media_type_guess: once_cell::sync::Lazy::new(|| mime_guess::from_path($relative_path)),
+            guess_media_type: {
+                fn get() -> mime_guess::MimeGuess {
+                    mime_guess::from_path($relative_path)
+                }
+                get
+            },
         }
     };
 }
@@ -15,14 +18,14 @@ initializer = 'initialize'
 "
 )]
 pub struct Asset {
-    media_type_guess: sync::Lazy<mime_guess::MimeGuess>,
+    guess_media_type: fn() -> mime_guess::MimeGuess,
 }
 
 pub fn main() {
     use base::examples::assets;
 
     assert_eq!(
-        *assets::CREDITS_MD.media_type_guess,
+        (assets::CREDITS_MD.guess_media_type)(),
         mime_guess::from_ext("md"),
     );
 }
