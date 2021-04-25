b86c82fba1cdd21a2b7fddbc3720adc6bfba084e
Benjamin Fischer <benjamin.fischer@evolutics.info>
2021-04-25 18:30:22 +0200

    Show key list in binary search example for clarity

--- examples/scenario_binary_search.rs
+++ examples/scenario_binary_search.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 #[iftree::include_file_tree("paths = '/examples/assets/**'")]
 pub struct Asset {
     relative_path: &'static str,
@@ -6,6 +8,18 @@ pub struct Asset {
 
 pub fn main() {
     let key_function = |asset: &Asset| asset.relative_path;
+
+    assert_eq!(
+        ASSETS.iter().map(key_function).collect::<vec::Vec<_>>(),
+        vec![
+            "examples/assets/.env",
+            "examples/assets/configuration/menu.json",
+            "examples/assets/configuration/translations.csv",
+            "examples/assets/credits.md",
+            "examples/assets/world/levels/tutorial.json",
+            "examples/assets/world/physical_constants.json",
+        ],
+    );
 
     let index = ASSETS.binary_search_by_key(&"examples/assets/credits.md", key_function);
     assert_eq!(index, Ok(3));
