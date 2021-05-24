58ce79713f2e48800f69a35928fc1713f1b0d198
Benjamin Fischer <benjamin.fischer@evolutics.info>
2021-05-24 11:13:58 +0200

    Panic immediately on error as everywhere in tests

--- tests/runs_dependent_project.rs
+++ tests/runs_dependent_project.rs
@@ -4,7 +4,7 @@ use std::process;
 
 #[test]
 fn main() {
-    let dependent = arrange().unwrap();
+    let dependent = arrange();
 
     let status = process::Command::new("cargo")
         .current_dir(dependent.path())
@@ -16,16 +16,16 @@ fn main() {
     dependent.close().unwrap();
 }
 
-fn arrange() -> Option<tempfile::TempDir> {
-    let dependent = tempfile::tempdir().ok()?;
-    arrange_manifest(&dependent)?;
-    arrange_source(&dependent)?;
-    Some(dependent)
+fn arrange() -> tempfile::TempDir {
+    let dependent = tempfile::tempdir().unwrap();
+    arrange_manifest(&dependent);
+    arrange_source(&dependent);
+    dependent
 }
 
-fn arrange_manifest(dependent: &tempfile::TempDir) -> Option<()> {
-    let dependency = env::var("CARGO_MANIFEST_DIR").ok()?;
-    let dependency = toml::to_string(&dependency).ok()?;
+fn arrange_manifest(dependent: &tempfile::TempDir) {
+    let dependency = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let dependency = toml::to_string(&dependency).unwrap();
     let contents = format!(
         "[package]
 name = 'dependent'
@@ -35,11 +35,11 @@ version = '0.1.0'
 iftree = {{ path = {} }}",
         dependency,
     );
-    fs::write(dependent.path().join("Cargo.toml"), contents).ok()
+    fs::write(dependent.path().join("Cargo.toml"), contents).unwrap();
 }
 
-fn arrange_source(dependent: &tempfile::TempDir) -> Option<()> {
-    fs::create_dir(dependent.path().join("src")).ok()?;
+fn arrange_source(dependent: &tempfile::TempDir) {
+    fs::create_dir(dependent.path().join("src")).unwrap();
     fs::write(
         dependent.path().join("src").join("main.rs"),
         "#[iftree::include_file_tree(\"paths = '/src/**'\")]
@@ -54,5 +54,5 @@ fn main() {
     assert_eq!(base::src::MAIN_RS.contents_str, include_str!(\"main.rs\"));
 }",
     )
-    .ok()
+    .unwrap();
 }
