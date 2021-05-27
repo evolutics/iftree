5cf4eef19ff1e2dc257b5e2596a31dccc563032e
Benjamin Fischer <benjamin.fischer@evolutics.info>
2021-05-27 22:08:37 +0200

    Add load test with many files and folders

---
+++ tests/handles_many_files.rs
@@ -0,0 +1,29 @@
+#[iftree::include_file_tree(
+    "
+paths = '**'
+base_folder = 'tests/many_files'
+"
+)]
+pub struct Asset {
+    relative_path: &'static str,
+    contents_str: &'static str,
+}
+
+#[test]
+fn main() {
+    assert_eq!(ASSETS.len(), 576);
+
+    assert_eq!(ASSETS[3].relative_path, "100");
+    assert_eq!(ASSETS[575].contents_str, "g/f/e/d/c/b/a/0\n");
+
+    for asset in &ASSETS {
+        assert_eq!(format!("{}\n", asset.relative_path), asset.contents_str);
+    }
+
+    assert_eq!(base::_127.relative_path, "127");
+    assert_eq!(base::e::c::b::_1.contents_str, "e/c/b/1\n");
+    assert_eq!(
+        base::g::f::e::d::c::b::a::_0.relative_path,
+        "g/f/e/d/c/b/a/0",
+    );
+}
