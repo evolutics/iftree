1a9a25c38626976e2f6f88c2a1c0c0ac704f355f
Benjamin Fischer <benjamin.fischer@evolutics.info>
2023-09-24 11:15:12 +0200

    Add container registry for Podman

--- scripts/test.py
+++ scripts/test.py
@@ -23,7 +23,7 @@ def _check_general_cleanliness():
             "--rm",
             "--volume",
             f"{working_folder}:/workdir",
-            "evolutics/travel-kit:0.8.0",
+            "docker.io/evolutics/travel-kit:0.8.0",
             "-c",
             "git ls-files -z | xargs -0 travel-kit check --",
         ],
